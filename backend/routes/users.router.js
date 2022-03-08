61458d6c2187c39917e58295e0bad22bcf56bfd7
tgcorias <ticianocorias@gmail.com>
2022-03-08 15:23:33 -0300

    [ADD] Users router

--- backend/routes/users.router.js
+++ backend/routes/users.router.js
@@ -7,6 +7,49 @@ router.get('/', (req, res) => {
     password: 'test',
   }
   res.json(users);
+});
+
+
+router.get('/:id', (req, res) => {
+  const id = req.params;
+  const users = {
+    id: 1,
+    email: 'Test',
+    password: 'test',
+  }
+  res.json(users);
+});
+
+
+router.post('/', (req, res) => {
+  const newUser = req.body;
+  const users = {
+    id: 1,
+    email: 'Test',
+    password: 'test',
+  }
+  res.json(users.push(newUser));
+});
+
+
+router.patch('/:id', (req, res) => {
+  const id = req.params;
+  const changes = req.body;
+  const users = changes.users;
+
+  res.json(users);
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params;
+  const users = {
+    id: 1,
+    email: 'Test',
+    password: 'test',
+  }
+  res.json(users);
 })
 
 module.exports = router;
+
+
